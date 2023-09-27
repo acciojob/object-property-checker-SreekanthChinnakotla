429fbc675d8120560d141e76cebceef58a9a9254
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-09-27 10:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  //   write your code 
+	return.sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
